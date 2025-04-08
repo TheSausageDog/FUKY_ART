40ba75adbee0c4e7dad064f564ef2ce75992b5b6
Paradox <zhi564133873k@126.com>
2025-04-08 21:42:24 +0800

    add foodtray step

---
+++ Assets/Scripts/Level/Tutorial/Level1/TutorialFoodTrayAppear.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialFoodTrayAppear : TutorialStep
+{
+    public GameObject foodTrayPrefab;
+
+    public override void Start()
+    {
+        base.Start();
+
+    }
+}
