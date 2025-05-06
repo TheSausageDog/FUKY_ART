e631dc29be3d223248e7670891d1bb59f70dd48b
Paradox <zhi564133873k@126.com>
2025-05-06 13:56:21 +0800

    heat food

--- Assets/Scripts/Item/CookTool/Pan.cs
+++ Assets/Scripts/Item/CookTool/Pan.cs
@@ -1,14 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Pan : MonoBehaviour
 {
+    protected bool heated = false;
+
+    protected HashSet<Food> foods = new HashSet<Food>();
+
+    void Update()
+    {
+        if (heated)
+        {
+            float heat = Time.deltaTime;
+            foreach (var food in foods)
+            {
+                food.Heat(heat);
+            }
+        }
+    }
+
     public void Heat()
     {
+        heated = true;
 
     }
 
     public void Unheat()
     {
+        heated = false;
+    }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Food>(out var food))
+        {
+            foods.Add(food);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<Food>(out var food) && foods.Contains(food))
+        {
+            foods.Remove(food);
+        }
     }
 }
--- Assets/Scripts/Item/Taste/Food.cs
+++ Assets/Scripts/Item/Taste/Food.cs
@@ -7,6 +7,7 @@ using BzKovSoft.ObjectSlicer;
 using BzKovSoft.ObjectSlicer.Samples;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /// <summary>
 /// 食物类，表示可切割的食物对象。
@@ -16,6 +17,29 @@ public class Food : TasteObject
 {
     public FoodType foodType; // 食物类型
     // public Color foodColor; // 食物颜色
+
+    public Material[] materials;
+
+    protected float heated = 0f;
+
+    void Awake()
+    {
+        Renderer[] renderers = GetComponents<Renderer>();
+        Renderer[] renderers2 = GetComponentsInChildren<Renderer>();
+        int length = renderers.Length + renderers2.Length;
+        materials = new Material[length];
+        int i = 0;
+        while (i < renderers.Length)
+        {
+            materials[i] = renderers[i].material;
+            ++i;
+        }
+        while (i < length)
+        {
+            materials[i] = renderers2[i - renderers.Length].material;
+            ++i;
+        }
+    }
 
     protected void SetTasteByType()
     {
@@ -52,6 +76,33 @@ public class Food : TasteObject
             base.Start();
         }
         // CalculateTaste();
+    }
+
+    public virtual void Heat(float _heat)
+    {
+        heated += _heat;
+        if (heated < 5)
+        {
+            foreach (var material in materials)
+            {
+                material.SetColor("_MainColor", Color.white);
+            }
+        }
+        else if (heated < 10)
+        {
+            foreach (var material in materials)
+            {
+                material.SetColor("_MainColor", new Color(138f / 255, 51 / 255, 36 / 255));
+            }
+        }
+        else
+        {
+            foreach (var material in materials)
+            {
+                material.SetColor("_MainColor", Color.white * 0.1f);
+            }
+        }
+
     }
 
 }
